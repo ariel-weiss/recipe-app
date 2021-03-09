9f188ded04172d575b9d008b845edcc600aa8611
Ariel Weiss <relweiss111@gmail.com>
2021-03-09 16:18:49 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const APP_ID = '69c8cf17';
+  const APP_KEY = '991ab8eb9fa41a1e9c59fac61b7edb4e';
+  const QUERY = 'chicken';
+  const request_template = `https://api.edamam.com/search?q=${QUERY}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='search-form'>
+        <input className='search-bar' type='text' />
+        <button className='search-button' type='submit'>Search</button>
+      </form>
     </div>
   );
 }
